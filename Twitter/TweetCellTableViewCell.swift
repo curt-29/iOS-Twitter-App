378f2e7b264134651a98320ac04bb2e8f820837f
Curtis Lee <cdlee17@students.desu.edu>
2019-02-13 17:03:07 -0500

    First Commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Curtis Lee on 2/12/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
